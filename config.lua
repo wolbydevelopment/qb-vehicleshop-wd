31ef57368b9bf2db584ee252776c6196a2ef425b
gutso37 <wolbydevelopment@gmail.com>
2021-04-29 10:18:00 +0300

    qb-vehicleshop
    This script is not made by us just released from another platform and members were requesting a lot and wanted to share.
    credit : Luminos Dev. and Kakarot

---
+++ config.lua
@@ -0,0 +1,29 @@
+-- WOLBY DEVELOPMENT https://discord.gg/cmcpXU5xtC -- 
+
+Config = {}
+
+-- WOLBY DEVELOPMENT https://discord.gg/cmcpXU5xtC -- 
+
+Config.PlateLetters  = 4
+
+-- WOLBY DEVELOPMENT https://discord.gg/cmcpXU5xtC -- 
+
+Config.PlateNumbers  = 4
+
+-- WOLBY DEVELOPMENT https://discord.gg/cmcpXU5xtC -- 
+
+Config.PlateUseSpace = false
+
+-- WOLBY DEVELOPMENT https://discord.gg/cmcpXU5xtC -- 
+
+Config.SpawnVehicle = true  -- TRUE if you want spawn vehicle when buy
+
+-- WOLBY DEVELOPMENT https://discord.gg/cmcpXU5xtC -- 
+
+Config.TestDrive = true     -- TRUE if you want enable test drive
+
+-- WOLBY DEVELOPMENT https://discord.gg/cmcpXU5xtC -- 
+
+Config.TestDriveTime = 30   -- TIME in SEC
+
+-- WOLBY DEVELOPMENT https://discord.gg/cmcpXU5xtC -- 
